d7acf29dbebeb5a6759fb0278bb35359b5e4028e
fzn0728 <thames.ft@gmail.com>
2016-11-21 10:03:57 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 10:00:12 2016
+
+@author: ZFang
+"""
+
+import pandas as pd
+import os
+
+if __name__ == '__main__':
+    ### file path
+    os.chdir(r'C:\Users\ZFang\Desktop\TeamCo\return and risk attribution project\\')
