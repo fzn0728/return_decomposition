0b66d1c4e4ff67f0420850210fd36ce2b0ff2bc1
fzn0728 <thames.ft@gmail.com>
2016-11-22 17:13:23 -0800

    wrap up

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov 21 10:00:12 2016
+Created on Tue Nov 22 15:01:23 2016
 
+@author: ZFang
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 21 10:00:12 2016
 @author: ZFang
 """
 
@@ -11,20 +17,29 @@ import matplotlib.pyplot as plt
 import statsmodels.formula.api as sm
 import numpy as np
 import outliers_influence as ou
+import copy
 
 def rolling_coef(df):
     para_df = pd.DataFrame(columns=['ACWI', 'MSCI_World', 'Russell_3000', 'US_Dollar', 'Bond_Index', 'HFRI_World_Index', 'HFRI_Relative_Value', 'HFRI_Macro', 'HFRI_Macro_Total', 'HFRI_ED', 'HFRI_EH'])
+    m_df = pd.DataFrame(columns=['ACWI', 'MSCI_World', 'Russell_3000', 'US_Dollar', 'Bond_Index', 'HFRI_World_Index', 'HFRI_Relative_Value', 'HFRI_Macro', 'HFRI_Macro_Total', 'HFRI_ED', 'HFRI_EH'])
     for i in range(0,len(df.index)-35):
         df_r = df.iloc[i:i+36,:]
-        my_ols = sm.ols(formula='Kroger ~ ACWI + MSCI_World + Russell_3000 + US_Dollar + Bond_Index + HFRI_World_Index + HFRI_Relative_Value + HFRI_Macro + HFRI_Macro_Total + HFRI_ED + HFRI_EH - 1', data=df_r).fit()
+        my_ols = sm.ols(formula='Kroger ~ ACWI + MSCI_World + Russell_3000 + US_Dollar + Bond_Index + HFRI_World_Index + HFRI_Relative_Value + HFRI_Macro + HFRI_Macro_Total + HFRI_ED + HFRI_EH', data=df_r).fit()
         # concat each rolling param
         s = pd.DataFrame(my_ols.params).T
+        m = pd.DataFrame(df_r.mean(axis=0)).T
         para_df = pd.concat([para_df,s], axis=0)
+        m_df = pd.concat([m_df,m], axis=0)
     # chean format of index
     para_df = para_df.reset_index()
     para_df = para_df.set_index(df.index[35:])
     para_df = para_df.drop('index', axis=1)
-    return para_df
+    
+    
+    m_df = m_df.reset_index()
+    m_df = m_df.set_index(df.index[35:])
+    m_df = m_df.drop('index', axis=1)
+    return m_df, para_df
 
     
 def get_error(df, reg):
@@ -49,7 +64,7 @@ def rolling_orth_coef(df):
         df_r['HFRI_ED_err'] = get_error(df_r, 'HFRI_ED ~ ACWI_err + MSCI_World_err + US_Dollar + Bond_Index + HFRI_World_Index_err + HFRI_Relative_Value_err + HFRI_Macro_err + HFRI_Macro_Total_err + HFRI_EH').values
         
     
-        my_ols = sm.ols(formula='Kroger ~ ACWI_err + MSCI_World_err + Russell_3000_err + US_Dollar + Bond_Index + HFRI_World_Index_err + HFRI_Relative_Value_err + HFRI_Macro_err + HFRI_Macro_Total_err + HFRI_ED_err + HFRI_EH - 1', data=df_r).fit()
+        my_ols = sm.ols(formula='Kroger ~ ACWI_err + MSCI_World_err + Russell_3000_err + US_Dollar + Bond_Index + HFRI_World_Index_err + HFRI_Relative_Value_err + HFRI_Macro_err + HFRI_Macro_Total_err + HFRI_ED_err + HFRI_EH', data=df_r).fit()
         # concat each rolling param
         s = pd.DataFrame(my_ols.params).T
         para_df = pd.concat([para_df,s], axis=0)
@@ -61,6 +76,7 @@ def rolling_orth_coef(df):
     
 def rolling_v_coef(df):
     para_df = pd.DataFrame(columns=['ACWI_err', 'MSCI_World_err', 'Russell_3000', 'US_Dollar', 'Bond_Index','HFRI_World_Index','HFRI_Relative_Value', 'HFRI_Macro_Total','HFRI_ED','HFRI_EH'])
+    m_df = pd.DataFrame(columns=['ACWI_err', 'MSCI_World_err', 'Russell_3000', 'US_Dollar', 'Bond_Index','HFRI_World_Index','HFRI_Relative_Value', 'HFRI_Macro_Total','HFRI_ED','HFRI_EH'])
     for i in range(0,len(df.index)-35):
         df_r = df.iloc[i:i+36,:]
         # gen orthogonal error series
@@ -68,15 +84,23 @@ def rolling_v_coef(df):
         df_r['MSCI_World_err'] = get_error(df_r, 'MSCI_World ~ ACWI_err + Russell_3000 + US_Dollar + Bond_Index + HFRI_World_Index + HFRI_Relative_Value + HFRI_Macro + HFRI_Macro_Total + HFRI_ED + HFRI_EH').values
         
     
-        my_ols = sm.ols(formula='Kroger ~ ACWI_err + MSCI_World_err + Russell_3000 + US_Dollar + Bond_Index + HFRI_World_Index + HFRI_Relative_Value + HFRI_Macro_Total + HFRI_ED + HFRI_EH - 1', data=df_r).fit()
+        my_ols = sm.ols(formula='Kroger ~ ACWI_err + MSCI_World_err + Russell_3000 + US_Dollar + Bond_Index + HFRI_World_Index + HFRI_Relative_Value + HFRI_Macro_Total + HFRI_ED + HFRI_EH', data=df_r).fit()
         # concat each rolling param
         s = pd.DataFrame(my_ols.params).T
+        m = pd.DataFrame(df_r.loc[:,['ACWI_err', 'MSCI_World_err', 'Russell_3000', 'US_Dollar', 'Bond_Index','HFRI_World_Index','HFRI_Relative_Value', 'HFRI_Macro_Total','HFRI_ED','HFRI_EH', 'Kroger']].mean(axis=0)).T
         para_df = pd.concat([para_df,s], axis=0)
+        m_df = pd.concat([m_df,m], axis=0)
     # chean format of index
     para_df = para_df.reset_index()
     para_df = para_df.set_index(df.index[35:])
     para_df = para_df.drop('index', axis=1)
-    return para_df
+    
+    
+    m_df = m_df.reset_index()
+    m_df = m_df.set_index(df.index[35:])
+    m_df = m_df.drop('index', axis=1)
+    # m_df.rename(columns={'ACWI_err':'ACWI', 'MSCI_World_err':'MSCI_World'})
+    return m_df, para_df
 
 
 
@@ -204,6 +228,24 @@ def cal_vif(df):
             VIF_df.iloc[i,j] = ou.variance_inflation_factor(df_.values,0)
     return VIF_df
     
+   
+def plot_pre(para_df, mean_df, title, column_name):
+    pred_df = copy.deepcopy(para_df)
+    for i in column_name:
+        pred_df[i] = para_df[i]*mean_df[i]    
+    pred_df['Kroger'] = mean_df['Kroger']
+    pred_df['BETA'] = pred_df.loc[:,column_name].sum(axis=1)
+    
+    fig, ax = plt.subplots()
+    plt.plot(pred_df['Kroger'].values, linestyle="-", linewidth=4, label='Kroger')
+    plt.plot(pred_df['Intercept'].values, linestyle="--", linewidth=2, label='Intercept')
+    plt.plot(pred_df['BETA'].values, linestyle="--", linewidth=2, label='BETA')
+    plt.legend()
+    plt.title(title)
+    
+    return pred_df
+
+
     
 if __name__ == '__main__':
     # file path
@@ -212,35 +254,33 @@ if __name__ == '__main__':
     df = pd.read_excel(file_name)
     df.index = df['Period']
     df = df.drop('Period', axis=1)
-    
     # Correlation 
     corr_df = df.corr()
     
+    
     ### Calculate VIF Matrix
     VIF_df = cal_vif(df)
-    
-    
     # Rolling 36 months Correlation
     r_36_df = df.rolling(window=36).corr()
     corr_36_df = r_36_df[df.index[36:]].values
     corr_36_Kroger_df = r_36_df.iloc[:,5].T
     corr_36_Kroger_df = corr_36_Kroger_df.iloc[35:]
-
     # Plot graph
     plt.style.use('fivethirtyeight')
     corr_36_Kroger_df.plot()
     plt.title('36 Months Rolling Correlation')
     
+    
     ### classical multi variable regerssion
-    para_df = rolling_coef(df)
+    mean_df, para_df = rolling_coef(df)
     # para_df.plot()
     # plt.title('Evolution of Coefficient - Classical Method')
-    
+    # Calculate Prediction
+    pred_df = plot_pre(para_df, mean_df, 'Prediction - Classical Method', ['ACWI', 'MSCI_World', 'Russell_3000', 'US_Dollar', 'Bond_Index', 'HFRI_World_Index', 'HFRI_Relative_Value', 'HFRI_Macro', 'HFRI_Macro_Total', 'HFRI_ED', 'HFRI_EH'])
     # Revise it into proportion
-    para_por_df = para_df
+    para_por_df = para_df.loc[:,['ACWI', 'MSCI_World', 'Russell_3000', 'US_Dollar', 'Bond_Index', 'HFRI_World_Index', 'HFRI_Relative_Value', 'HFRI_Macro', 'HFRI_Macro_Total', 'HFRI_ED', 'HFRI_EH']]
     para_por_df = para_por_df.apply(lambda x: x/x.sum(), axis=1)
-    
-    para_por_abs_df = para_df.abs()
+    para_por_abs_df = para_df.loc[:,['ACWI', 'MSCI_World', 'Russell_3000', 'US_Dollar', 'Bond_Index', 'HFRI_World_Index', 'HFRI_Relative_Value', 'HFRI_Macro', 'HFRI_Macro_Total', 'HFRI_ED', 'HFRI_EH']].abs()
     para_por_abs_df = para_por_abs_df.apply(lambda x: x/x.sum(), axis=1)
     # plot
     # plot_stack(para_por_df)
@@ -252,12 +292,10 @@ if __name__ == '__main__':
     para_orth_df = rolling_orth_coef(df)
     # para_orth_df.plot()
     # plt.title('Evolution of Coefficient - Orthogonal Method')
-
     # Revise it into proportion
-    para_por_orth_df = para_orth_df
+    para_por_orth_df = para_orth_df.loc[:,['ACWI_err', 'MSCI_World_err', 'Russell_3000_err', 'US_Dollar', 'Bond_Index','HFRI_World_Index_err','HFRI_Relative_Value_err','HFRI_Macro_err','HFRI_Macro_Total_err','HFRI_ED_err','HFRI_EH']]
     para_por_orth_df = para_por_orth_df.apply(lambda x: x/x.sum(), axis=1)
-    
-    para_por_orth_abs_df = para_orth_df.abs()
+    para_por_orth_abs_df = para_orth_df.loc[:,['ACWI_err', 'MSCI_World_err', 'Russell_3000_err', 'US_Dollar', 'Bond_Index','HFRI_World_Index_err','HFRI_Relative_Value_err','HFRI_Macro_err','HFRI_Macro_Total_err','HFRI_ED_err','HFRI_EH']].abs()
     para_por_orth_abs_df = para_por_orth_abs_df.apply(lambda x: x/x.sum(), axis=1)
     # plot
     # plot_orth_stack(para_por_orth_df)
@@ -265,15 +303,15 @@ if __name__ == '__main__':
     
     
     ### VIF adjusted method
-    para_v_df = rolling_v_coef(df)
+    mean_v_df, para_v_df = rolling_v_coef(df)
     # para_v_df.plot()
     # plt.title('Evolution of Coefficient - VIF adjusted Method')
-
+    # Calculate Prediction
+    pred_v_df = plot_pre(para_v_df, mean_v_df, 'Prediction - VIF Adjusted Method', ['ACWI_err', 'MSCI_World_err', 'Russell_3000', 'US_Dollar', 'Bond_Index', 'HFRI_World_Index', 'HFRI_Relative_Value', 'HFRI_Macro_Total', 'HFRI_ED', 'HFRI_EH'])
     # Revise it into proportion
-    para_por_v_df = para_v_df
-    para_por_v_df = para_por_v_df.apply(lambda x: x/x.sum(), axis=1)
-    
-    para_por_v_abs_df = para_v_df.abs()
+    para_por_v_df = para_v_df.loc[:,['ACWI_err', 'MSCI_World_err', 'Russell_3000', 'US_Dollar', 'Bond_Index','HFRI_World_Index','HFRI_Relative_Value', 'HFRI_Macro_Total','HFRI_ED','HFRI_EH']]
+    para_por_v_df = para_por_v_df.apply(lambda x: x/x.sum(), axis=1)    
+    para_por_v_abs_df = para_v_df.loc[:,['ACWI_err', 'MSCI_World_err', 'Russell_3000', 'US_Dollar', 'Bond_Index','HFRI_World_Index','HFRI_Relative_Value', 'HFRI_Macro_Total','HFRI_ED','HFRI_EH']].abs()
     para_por_v_abs_df = para_por_v_abs_df.apply(lambda x: x/x.sum(), axis=1)
     # plot
     # plot_v_stack(para_por_v_df)
@@ -282,14 +320,24 @@ if __name__ == '__main__':
     
     
     
-'''
-    my_ols = sm.ols(formula='HFRI_Macro ~ HFRI_Macro_Total', data=df).fit()
-    VIF_l = []
-    for i in range(0,12):
-        VIF = ou.variance_inflation_factor(df.values,i)
-        VIF_l.append(VIF)
-'''
+    ### other more plots
+    
+    pred_v_df.plot(linewidth=2)
+    plt.legend(loc='upper right',prop={'size':8})
+    
+    fig, ax = plt.subplots()
+    date = np.arange(66)
+    Alpha = pred_v_df['Intercept'].values
+    Kroger = pred_v_df['Kroger'].values
+    
+    # plt.plot([],[], label='Intercept', color=')
+    # plt.plot([],[], label='Kroger', color='r')
+    plt.stackplot(date, Alpha, Kroger, alpha=0.7)
+
     
     
+
+
+
 
     
